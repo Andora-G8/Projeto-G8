5272da0768879a14a3d39363f71b0270a8fa3a87
Thiago Araújo <112591325+tharaujo17@users.noreply.github.com>
2022-11-13 14:21:13 -0300

    alterações para o sistema de cadastro

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import os
 os.system('cls')
 
 from bem_vindo import boas_vindas
+from cadastro_usuario import registro
 from shopping import selecionar_shopping
 from loja import escolher_loja
 from vaga import selecionar_vaga
 from confirmar import confirmar_vaga
 
 bem_vindo = boas_vindas()
+registro = registro()
 escolha_shopping = selecionar_shopping()
 escolha_loja = escolher_loja(escolha_shopping)
 vaga = selecionar_vaga(escolha_shopping, escolha_loja)
